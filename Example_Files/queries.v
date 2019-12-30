9e722e6183ede34cc0a40ab91dc55046a3a68ab9
amypritc <amypritc@buffalo.edu>
2019-12-29 23:53:48 -0500

    small fix

---
+++ Example_Files/queries.v
@@ -0,0 +1,54 @@
+
+Search plus.
+
+Search plus 0.
+
+Search (~ _ <-> _ ).
+
+Search plus Nat.odd.
+
+Require Import Arith.
+
+Search plus Nat.odd.
+
+SearchRewrite ( _ * _ / _ ).
+
+
+
+Check 0.
+
+Check nat.
+
+Check plus.
+
+Check plus 2 3.
+
+
+
+About plus.
+
+
+
+Locate plus.
+
+Locate nat.
+
+
+
+Print plus.
+
+Print Libraries.
+
+
+
+Print Assumptions plus.
+
+
+
+Compute 2 + 4.
+
+Compute plus 4 2.
+
+Compute (mult 12 4) * 3.
+
+
